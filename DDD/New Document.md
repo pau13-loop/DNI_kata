286bc975e95be64c3e5a4ef3254e4968d15ec73e
pau <pllinas@cifpfbmoll.eu>
2021-01-23 17:49:55 +0100

    feat: :zap: We adjust our program to a better implementation of the OOP paradigm

---
+++ DDD/New Document.md
@@ -0,0 +1,12 @@
+# Domain Concepts
+---
+<dl>
+  <dt><strong>Dni</strong></dt>
+  <dd>National Identification Document, is composed by a serie of 8 digits and an alphabetic letter.</dd>
+  <dt><strong>Dni number</strong></dt>
+  <dd>Is composed by a serie of 8 digits</dd>
+  <dt><strong>Nif letter</strong></dt>
+  <dd>Is the letter that each DNI has assigned after the after the 8 digits serie</dd>
+  <dt><strong>Allocation Table</strong></dt>
+  <dd>A table that includes all the possible alphabetic letter that can be selected for each DNI</dd>
+</dl>
--- domain/src/main.py
+++ domain/src/main.py
@@ -1,30 +1,41 @@
-from src.assign_table import assignTable
-
-
-class DNI():
+class Dni():
 
     def __init__(self, number):
         self.number = number
+        # Allowed dni numbers
+        self.valid_numbers = '0123456789'
+        # Length of the numbers of the Dni without a letter
+        self.length_numbers_dni = 8
+        # Length of the numbers of the Dni with a letter
+        self.length_dni = 9
+        # Table with the allowed characters for the Dni
+        self.allocation_table = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
+                             'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
 
-    def dni_validation(self):
+    def set_dni(self):
+        if Dni.validate_dni_number(self) != True:
+            return 'Try again please !'
+        letter = AllocationTable.get_nif_letter(self)
+        return self.number + letter
+
+    def validate_dni_number(self):
         for num in self.number:
-            if num in assignTable.number:
+            if num in self.valid_numbers:
                 continue
             else:
                 return False
-        if len(self.number) == assignTable.length_numbers_dni:
+        if len(self.number) == self.length_numbers_dni:
             return True
         else:
             return False
 
-    def get_letter(self):
-        if self.dni_validation() == True:
-            letter_position = int(self.number) % 23
-            return assignTable.table[letter_position]
-        else:
-            return False
 
-    def get_dni(self):
-        if self.get_letter() != False:
-            return str(self.number) + str(self.get_letter())
-        return 'Try again please !'
+class AllocationTable(Dni):
+
+    def __init__(self, number):
+        Dni.__init__(self, number)
+
+    def get_nif_letter(self):
+        letter_position = int(self.number) % 23
+        letter = self.allocation_table[letter_position]
+        return letter
